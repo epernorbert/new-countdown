9060d1573ebcbdb1c0509f4358110b3e165bbc34
epernorbert <eper.norbert98@gmail.com>
2023-09-02 15:50:23 +0200

    Add Admin page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Admin from './Pages/Admin/Admin';
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
     <div className="App">      
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </div>
   )
---
+++ client/src/Pages/Admin/Admin.tsx
@@ -0,0 +1,11 @@
+import styles from './Admin.module.scss';
+
+const Admin = () => {  
+  return (
+    <div className={styles.admin}>
+      <h1>This is admin page</h1>
+    </div>   
+  );
+};
+
+export default Admin;
