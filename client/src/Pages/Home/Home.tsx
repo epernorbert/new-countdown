cce8453ff2b53262708fd1cbeed7b8b39028bbd6
epernorbert <eper.norbert98@gmail.com>
2023-11-12 12:05:40 +0100

    Display message if controller already taken or not

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Home.module.scss";
 
 type Props = {};
 
+type Controller = {
+  controller_id: number;
+  controller_name: string;
+};
+
 const Home = ({}: Props) => {
   const [room, setRoom] = useState("");
+  const [controller, setController] = useState([]);
+  const [isControllerExist, setIsControllerExist] = useState(false);
 
   const handleCreateRoom = () => {
     fetch(`http://localhost:5000/create-room/${room}`, {
@@ -14,6 +21,24 @@ const Home = ({}: Props) => {
         room: room,
       }),
     }).then((response) => response.json());
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/controller-list")
+      .then((response) => response.json())
+      .then((data) => setController(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    const res = controller.find(findController);
+    typeof res === "object"
+      ? setIsControllerExist(true)
+      : setIsControllerExist(false);
+  }, [room]);
+
+  const findController = (controller: Controller) => {
+    return controller.controller_name === room;
   };
 
   return (
@@ -31,6 +56,11 @@ const Home = ({}: Props) => {
           Create room
         </a>
       </div>
+      {room.length === 0 && <p>Start typing...</p>}
+      {room.length > 0 && !isControllerExist && (
+        <p style={{ color: "green" }}>FREE</p>
+      )}
+      {isControllerExist && <p style={{ color: "red" }}>TAKEN</p>}
     </div>
   );
 };
